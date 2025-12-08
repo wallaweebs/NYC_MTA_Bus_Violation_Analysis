cbaabaa8d360e46177e4f12e5ef894e32163836f
addinar <raddina24@gmail.com>
2025-12-08 18:17:47 -0500

    added primary EDA notebook

---
+++ main/src/charts/Chart.jsx
@@ -0,0 +1,9 @@
+// all recharts go in this subdirectory
+
+// note for debasree:
+
+// this may seem obvious but if you want to make 
+// a reusable chart component then put it in components folder
+
+// i just made a temp Chart.jsx file here so that the folder
+// actually shows up on github 
