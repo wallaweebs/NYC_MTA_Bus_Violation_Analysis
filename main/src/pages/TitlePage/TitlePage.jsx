92085ab1643aa7ba7e3422a955e5b381025775a2
addinar <raddina24@gmail.com>
2025-12-09 11:24:17 -0500

    revise title slide

--- main/src/pages/TitlePage/TitlePage.jsx
+++ main/src/pages/TitlePage/TitlePage.jsx
@@ -6,9 +6,11 @@ export default function TitlePage() {
             <div className="big">
                 <div className="big1">
                     <div className="big2">
-                        <h1 className="title radial-text-1">How do bus lane violations vary by NYC neighborhood?</h1>
-                        <h2 className="subtitle">Visualizations Final Project by Team GOYAL</h2>
-                        <h3 className="names">Addina Rahaman, Debasree Sen, Sadeq Abubaker Alhanshali</h3>
+                        <div className="text-wrapper">
+                            <h1 className="title radial-text-1">How do bus lane violations vary by NYC neighborhood?</h1>
+                            <h2 className="subtitle radial-text-2">Visualizations Final Project by Team GOYAL</h2>
+                            <h3 className="names radial-text-2">Addina Rahaman, Debasree Sen, Sadeq Abubaker Alhanshali</h3>
+                        </div>
                     </div>
                 </div>
             </div>
